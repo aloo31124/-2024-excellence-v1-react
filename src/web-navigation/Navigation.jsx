11b818add25cf29a623d3991fbb5ce06b1a0cd13
loulou <aloo31124@gmail.com>
2024-05-04 19:16:07 +0800

    feat [公司形象網站] react 建構 => nav 引入圖 svg

--- src/web-navigation/Navigation.jsx
+++ src/web-navigation/Navigation.jsx
@@ -1,5 +1,10 @@
 
 import './Navigation.css'
+import logo from '../assets/logo/logo.svg';
+import iconHandShake from '../assets/icon/icon_handshake.svg';
+import iconMenu from '../assets/icon/icon_handshake.svg';
+import icon2Img from '../assets/icon/icon_2_img.svg';
+import iconPhone from '../assets/icon/icon_handshake.svg';
 
 function Navigation() {
     
@@ -7,22 +12,26 @@ function Navigation() {
 
             <div className="nav-container">
                 <div className="nav-centent">
+                    <a href="#" id="logo" className="logo">
+                        <img src={logo} />
+                    </a>
                     <div className="item-list">
                         <a className="item" id="nav-item1" href="#price-work-flow">
+                            <img src={iconHandShake} />
                             <label>合作流程</label>
                         </a>
                         <a className="item" id="nav-item2" href="#service-list">
-                            
+                            <img src={iconMenu} />
                             <label>服務項目</label>
                         </a>
                         <a className="item" id="nav-item3" href="#portfolio-block">
                             &nbsp;
-                            
+                            <img src={icon2Img} />
                             <label>作品集</label>
                             &nbsp;
                         </a>
                         <a className="item" id="nav-item4" href="#footer">
-                            
+                            <img src={iconPhone} />
                             <label>聯絡我們</label>
                         </a>
                     </div>
