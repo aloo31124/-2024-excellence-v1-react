0cb277d8ba6efa4d0b0d41dea6b62bb4ed81e904
loulou <aloo31124@gmail.com>
2024-05-04 15:03:25 +0800

    [公司形象網站] => first react 建構 => nav 組件

---
+++ src/web-navigation/Navigation.jsx
@@ -0,0 +1,40 @@
+
+import './Navigation.css'
+
+function Navigation() {
+    
+    return (
+        <nav className="nav-container">
+
+            <div className="text-bounce-bottom-container">
+                <div className="nav-centent">
+                    <div className="item-list">
+                        <a className="item" id="nav-item1" href="#price-work-flow">
+                            <label>合作流程</label>
+                        </a>
+                        <a className="item" id="nav-item2" href="#service-list">
+                            
+                            <label>服務項目</label>
+                        </a>
+                        <a className="item" id="nav-item3" href="#portfolio-block">
+                            &nbsp;
+                            
+                            <label>作品集</label>
+                            &nbsp;
+                        </a>
+                        <a className="item" id="nav-item4" href="#footer">
+                            
+                            <label>聯絡我們</label>
+                        </a>
+                    </div>
+                </div>
+                <div className="text-bounce-bottom-box">
+                    <div className="text-bounce-bottom"></div>
+                </div>
+            </div>
+        </nav>
+    );
+}
+
+export default Navigation
+
