cc2bd728bef897effb7d5e9f4b7483331513b3f7
loulou <aloo31124@gmail.com>
2024-05-05 09:45:20 +0800

    [公司形象網站] => react 建構 => nav 動畫

--- src/web-navigation/Navigation.jsx
+++ src/web-navigation/Navigation.jsx
@@ -1,4 +1,4 @@
-
+import React, { useState } from 'react';
 import './Navigation.css'
 import logo from '../assets/logo/logo.svg';
 import iconHandShake from '../assets/icon/icon_handshake.svg';
@@ -7,36 +7,45 @@ import icon2Img from '../assets/icon/icon_2_img.svg';
 import iconPhone from '../assets/icon/icon_handshake.svg';
 
 function Navigation() {
-    
+    const [isOpen, setIsOpen] = useState(true); // 初始狀態為展開
+
+    const toggleNav = () => {
+      setIsOpen(!isOpen);
+    };
+
     return (
 
             <div className="nav-container">
-                <div className="nav-centent">
-                    <a href="#" id="logo" className="logo">
+                <div className={`nav-centent ${isOpen ? '' : 'closed'}`}>
+                    <a href="#" id="logo" className={`logo ${isOpen ? '' : 'closed'}`}>
                         <img src={logo} />
                     </a>
-                    <div className="item-list">
-                        <a className="item" id="nav-item1" href="#price-work-flow">
+                    <div className={`item-list ${isOpen ? '' : 'closed'}`}>
+                        <a className="item" href="#price-work-flow">
                             <img src={iconHandShake} />
                             <label>合作流程</label>
                         </a>
-                        <a className="item" id="nav-item2" href="#service-list">
+                        <a className="item" href="#service-list">
                             <img src={iconMenu} />
                             <label>服務項目</label>
                         </a>
-                        <a className="item" id="nav-item3" href="#portfolio-block">
+                        <a className="item" href="#portfolio-block">
                             &nbsp;
                             <img src={icon2Img} />
                             <label>作品集</label>
                             &nbsp;
                         </a>
-                        <a className="item" id="nav-item4" href="#footer">
+                        <a className="item" href="#footer">
                             <img src={iconPhone} />
                             <label>聯絡我們</label>
                         </a>
+                        <div className="item fa fa-close" onClick={toggleNav}></div>
+                    </div>
+                    <div className={`item-bar ${isOpen ? '' : 'closed'}`} >
+                        <div className="fa fa-bars" onClick={toggleNav}></div>
                     </div>
                 </div>
-                <div className="text-bounce-bottom-box">
+                <div className={`text-bounce-bottom-box ${isOpen ? '' : 'closed'}`}>
                     <div className="text-bounce-bottom"></div>
                 </div>
             </div>
